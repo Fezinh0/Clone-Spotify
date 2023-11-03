e35caff8dae18548c8749a7768591fcb23b00c3c
Fernando Oliveira <104396976+Fezinh0@users.noreply.github.com>
2023-11-03 18:08:35 -0300

    Fisrt Commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18d760',
+        'purple-main': '#2d4689',
+        'green-main': '#1ed760'
+      },
+      backgroundImage: theme => ({
+        'spotify-theme' : "url('/src/img/bursts.svg')",
+        'spotify-theme-mobile' : "url('/src/img/bursts-mobile.svg')",
+  
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner':'46% 4%',
+        'banner-mobile':' top 25% center'
+      },
+      fontSize: {
+        '9xl':'9rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
